5620a6786a1429d5020215b5963d143c4976515f
damon_gege <z3439119@gmail.com>
2024-06-21 13:53:24 +1000

    add web analytics

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -2,6 +2,7 @@
 import NavBar from "@/components/Layout/NavBar";
 import { FollowerProvider } from "@/lib/MouseFollower";
 import { Sono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 const inter = Sono({ subsets: ["latin"] });
 export default function HomeLayout({
   children, // will be a page or nested layout
@@ -11,6 +12,7 @@ export default function HomeLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         <FollowerProvider visibleScreenWidth={640}>
           <NavBar />
           <section className=" bg-transparent mt-4">{children}</section>
